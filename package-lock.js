46d931b54264861a8b04608510a2a811a60e095b
hijin <tme529110@gmail.com>
2026-09-24 18:26:42 +0800

    Create package-lock.js

---
+++ package-lock.js
@@ -0,0 +1,15 @@
+{
+  "name": "qvac-mini-research-desk",
+  "version": "1.0.0",
+  "lockfileVersion": 3,
+  "requires": true,
+  "packages": {
+    "": {
+      "name": "qvac-mini-research-desk",
+      "version": "1.0.0",
+      "dependencies": {
+        "express": "^5.1.0"
+      }
+    }
+  }
+}
